831829176c72666bf513eb4af14e3228d7d2be52
USUARIO <USUARIO@DESKTOP-PUPJLV2>
2022-06-12 14:03:24 -0300

    v1

---
+++ TP5 Ejercicio 6/src/tp5/ejercicio/pkg6/TP5Ejercicio6.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp5.ejercicio.pkg6;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class TP5Ejercicio6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*
+        String frases[] = new String[25];
+        System.out.println(frases[3].length());
+         
+        Saltara una excepcion por que declaramos una variable este caso un vector
+        pero no creamos objetos dentro del mismo.
+        En este caso no se creo un objeto en la posiscion 3, por lo tanto no se
+        puede obtener el tamano de algo que no existe.
+        El tipo de excepcion es "" NullPointerException "".
+        */
+        
+        /*
+        String aux = "Hola";
+        int aux2 = Integer.parseInt(aux);
+        
+        Saltara una excepcion por combertimos un String a un int, esto no se puede
+        realizar por la cadena no tiene el formato adecuado;
+        El tipo de excepcion es "" NumberFormatException "".
+        */
+    }
+    
+}
