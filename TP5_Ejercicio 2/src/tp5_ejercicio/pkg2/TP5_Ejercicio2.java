1bad603466aec79bd6ea0d4d7aec272891e1daae
Matias <Matias@LAPTOP-PVUVC0VL>
2022-06-13 17:11:50 -0300

    TP5_Ejercicio6 Excepción

--- TP5_Ejercicio 2/src/tp5_ejercicio/pkg2/TP5_Ejercicio2.java
+++ TP5_Ejercicio 2/src/tp5_ejercicio/pkg2/TP5_Ejercicio2.java
@@ -8,7 +8,10 @@ public class TP5_Ejercicio2 {
 
     
     public static void main(String[] args) {
-        //Empleado empleados[] = new Empleado[3];
+        Empleado empleados[] = new Empleado[3];
+        
+        empleados[0] = new Empleado("juan","Lopez",LocalDate.of(2005,06,12),34657456,2020);
+        
         
     }
     
---
+++ TP5_Ejercicio 6/src/tp5_ejercicio/pkg6/TP5_Ejercicio6.java
@@ -0,0 +1,24 @@
+
+package tp5_ejercicio.pkg6;
+
+import java.util.Arrays;
+
+public class TP5_Ejercicio6 {
+
+    
+    public static void main(String[] args) {
+    /*try{
+    }
+    catch(Exception e){
+         System.out.println(e.getMessage());
+    }*/
+    
+    String frases[] = new String[25];
+    System.out.println(frases[3].length());
+    
+    String aux = "Hola";
+    int aux2 = Integer.parseInt(aux);
+        
+    }
+    
+}
